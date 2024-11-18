cb2950b57a72a6703b74bf1a56b9741c2f97392e
teddy <teddystwn48@gmail.com>
2024-11-18 13:00:43 +0700

    update

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import { env } from "bun";
 import { app } from "./src/index";
 const { PORT } = env;
 import { handle } from "hono/vercel";
+
 const handler = handle(app);
 export const POST = handler;
 export const PATCH = handler;
 export const PUT = handler;
 export const OPTIONS = handler;
-export default { port: PORT, fetch: app.fetch };
 export const GET = handler;
+
+export default { port: PORT, fetch: app.fetch };
+
 console.log("app start with port " + PORT);
